c0f6e569480eca91325f479c290d2de400a4395f
Fernanda Souza Oliveira <fernanda.oliveira@dbserver.com.br>
2026-01-02 23:27:16 -0300

    Funcao de Cadastrar e Listar Contatos

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -5,6 +6,13 @@ public class Main {
         //Variaveis de Controle
         Scanner scanner = new Scanner(System.in);
         String inputUsuario;
+        int idContato = 1;
+        String nomeContato;
+        String telefoneContato;
+
+
+        //Lista de contatos
+        ArrayList<Contato> contatos = new ArrayList<>();
 
 
         //Menu Inicial
@@ -22,7 +30,35 @@ public class Main {
 
             switch (inputUsuario){
                 case "1":
-                    System.out.println("Contato Criado");
+                    System.out.println("\n Digite o Nome do Contato: ");
+                    nomeContato = scanner.nextLine().trim();
+
+                    while (!Contato.nomeValido(nomeContato)){
+                        System.out.println("Nome inválido. Digite novamente:");
+                        nomeContato = scanner.nextLine().trim();
+                    }
+
+                    System.out.println("\n Digite o Numero de telefone do Contato: ");
+                    telefoneContato = scanner.nextLine().trim();
+
+                    while (!Telefone.numTelValido(telefoneContato)){
+                        System.out.println("Número inválido. Digite um numero de telefone valido com 9 até 14 digitos:");
+                        telefoneContato = scanner.nextLine().trim();
+                    }
+
+                    Telefone telefone = new Telefone();
+
+                    telefone.setNumTelefone(telefoneContato);
+
+                    Contato contato = new Contato();
+
+                    contato.setId(idContato++);
+                    contato.setNome(nomeContato);
+                    contato.setNumTelefone(telefone);
+
+                    contatos.add(contato);
+
+                    System.out.println("\n Contato Adicionado com Sucesso!");
                     break;
 
                 case "2":
@@ -31,6 +67,15 @@ public class Main {
 
                 case "3":
                     System.out.println("Contatos Listados");
+                    if (contatos.isEmpty()) {
+                        System.out.println("Nenhum contato cadastrado.");
+                    } else {
+                        System.out.println("\nLista de Contatos:");
+                        //Percorre a lista e imprime cada objeto pessoa
+                        for (Contato c : contatos) {
+                            System.out.println(c);
+                        }
+                    }
                     break;
 
                 case "4":
